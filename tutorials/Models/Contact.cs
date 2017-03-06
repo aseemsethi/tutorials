f1261a4f3a20b3f337846e47aead4596708162d4
Aseem Sethi <aseemsethi@yahoo.com>
2017-03-06 15:13:59 +0530

    Added a Model DB

---
+++ tutorials/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tutorials.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public Guid userId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string streetAddress1 { get; set; }
+        public string streetAddress2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
